11e03c79bea3e7900b53be987b47fe6a0d93b07a
Nimish Santosh <nimishsantosh107@icloud.com>
2018-12-05 11:02:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('toCaps' , (text)=>{
+	return text.toUpperCase();
+});
+
+app.set('view engine' , 'hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+	let date = new Date().toString();
+	let log = `${date}:  REQUEST: ${req.url}`
+	console.log(log);
+	res.render('maintenance.hbs');});
+
+app.get('/' , (req,res)=>{
+	var currentDate = new Date();
+	res.render('home.hbs' ,{
+		pageTitle: `Home Page`,
+		welcomeMessage: `Welcome to home page`,
+		capsMessage: `this is the home page`,
+		currentYear: currentDate.getFullYear(),
+	});})
+
+app.get('/about' , (req,res)=>{
+	var currentDate = new Date();
+	res.render('about.hbs', {
+		pageTitle: 'About page',
+		currentYear: currentDate.getFullYear(),
+	});});
+
+app.get('/bad' , (req,res)=>{
+	res.send({
+		error: "404 PAGE AIN'T FOUND"
+	});});
+
+app.listen(3000 , ()=>{
+	console.log("SERVER IS UP ON PORT: 3000");
+});
